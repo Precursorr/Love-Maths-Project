d788c959db660fd04d38ca4b57dab83fd6cf6e86
Denis Berrill <denisberrill98@gmail.com>
2024-06-16 20:08:47 +0000

    Add multiplication game code

--- assests/js/script.js
+++ assests/js/script.js
@@ -30,6 +30,8 @@ function runGame(gameType) {
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
+    } else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
     } else {
         alert(`unknown game type: ${gameType}`);
         throw `unknown game type: ${gameType}. Aborting!`;
@@ -47,10 +49,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You got it right! :D");
-        incrementScore()
+        incrementScore();
     } else {
         alert(`Awwww... You answered ${userAnswer}. The correct answer is ${calculatedAnswer[0]}!`);
-        incrementWrongAnswer()
+        incrementWrongAnswer();
     }
     runGame(calculatedAnswer[1]);
 }
@@ -64,8 +66,10 @@ function calculateCorrectAnswer() {
     let operand2 = parseInt(document.getElementById('operand2').innerText);
     let operator = document.getElementById("operator").innerText;
 
-    if (operator = "+") {
+    if (operator === "+") {
         return [operand1 + operand2, "addition"];
+    } else if (operator === "x") {
+        return [operand1 * operand2, "multiply"];
     } else {
         alert(`unimplemented operator ${operator}`);
         throw `unimplemented operator ${operator}. Aborting!`;
@@ -103,8 +107,10 @@ function displaySubtractQuestion() {
 
 }
 
-function displayMultiplyQuestion() {
-    
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById('operand1'). textContent = operand1;
+    document.getElementById('operand2'). textContent = operand2;
+    document.getElementById('operator'). textContent = "x";
 }
 
 function displayDivisionQuestion() {
